883c03c8d8d0e9c2398926b92406af4a0f92485e
shahrul <shahrulefz@gmail.com>
2022-11-02 20:45:32 +0800

    added tailwind & test hero banner

---
+++ src/components/HeroBanner.js
@@ -0,0 +1,14 @@
+const Hero = () => {
+
+    return (
+        <section
+            className="bg-white"
+        >
+            <div className="container-sm uppercase">
+                Test
+            </div>
+        </section>
+    );
+}
+
+export default Hero;
